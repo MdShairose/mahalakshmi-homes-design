025f33aea9c9abc65cff46562b1dbd5bc13cb59c
Mohammad Shairose Ahmed <ashloo.ahmed@gmail.com>
2021-08-09 22:00:06 +0530

    commits

--- script/completed.js
+++
@@ -1,6 +0,0 @@
-function showmore() {
-                    document.getElementById('three').style.display = 'block';
-                    document.getElementById('four').style.display = 'block';
-                    document.getElementById('see').disabled = true;
-                    document.getElementById("see").className = "disabled";
-}
