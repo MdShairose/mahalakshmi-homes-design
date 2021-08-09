a8d883f1052d61089920a19bea26734aff67f150
Mohammad Shairose Ahmed <ashloo.ahmed@gmail.com>
2021-08-09 16:23:03 +0530

    ongoing

---
+++ script/completed.js
@@ -0,0 +1,6 @@
+function showmore() {
+                    document.getElementById('three').style.display = 'block';
+                    document.getElementById('four').style.display = 'block';
+                    document.getElementById('see').disabled = true;
+                    document.getElementById("see").className = "disabled";
+}
