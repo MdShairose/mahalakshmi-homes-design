1dbb928e3aecd1a235a060fa5396127eec25b462
Rahul <46596909+Rahulvamshi@users.noreply.github.com>
2021-09-14 12:46:35 +0530

    2nd slide show done

--- script/home.js
+++ script/home.js
@@ -1,7 +1,19 @@
-let slideIndex = 1,present,myVar;
+let slideIndex = 0,present,myVar;
 let matters = ["<p>Located in the most desired area of <br>Manikonda, Mahalakshmi’s Avasa has <br>3-Bedroom and 2-Bedroom flats ranging <br>from 1100 sft to 1700 sft. The apartment is <br>nearby Gachibowli IT corridor and Financial <br>District 1.</p>" ,
                "<p>Located in Bachupally, Mahalakshmi’s <br>Astoria offers 3-Bedroom and 2- Bedroom <br>flats ranging from 1125 sft to 1350 sft. <br>The apartment’s proximity to engineering <br>colleges and international schools makes it a <br>very desirable location and an easy <br>commute to Hi-Tech city.</p>",
                "<p>Spread across 8 Acres, located on <br>Karakambadi Road, Tirupati has 50 luxurious <br>villas that are East, West and North <br>Facing ranging from 2200sft to 3600sft. <br>Conveniently located within 10 mins from Bus <br>stand and has all the amenities including <br>a club house, swimming pool and <br>children’s play area.</p>"];
+
+let i=1;
+
+var counter = 1;
+setInterval(function(){
+  next();
+  counter++;
+  if(counter > 3){
+    counter = 1;
+  }
+}, 10000);
+
 
 function next()
 {
@@ -49,8 +61,5 @@ function next()
             break;
   }
 
-<<<<<<< HEAD
-  myVar = setTimeout(showSlides, 1000);
-=======
->>>>>>> 7325e3571ab813126d2b79314fa647b112b0b2ca
+
 }
